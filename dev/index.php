0e302d5435c21e1ebf91020718eb1119c1b1c4ff
ilcomp <cgde88@gmail.com>
2015-10-13 16:29:37 +0300

    u

---
+++ dev/index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>kiss</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <link href="/favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon">
+
+<!-- build:remove -->
+	<link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700,800&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=PT+Serif:400,700&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+<!-- /build -->
+<!-- build:css inline ../../../dev/css/meta.min.css -->
+	<link href="/css/meta.css" rel="stylesheet">
+<!-- /build -->
+<!-- build:remove -->
+	<link href="/css/style.css" rel="stylesheet">
+<!-- /build -->
+<!-- build:template
+	<script>
+	(function(u){function loadCSS(e,t,n){"use strict";function r(){for(var t,i=0;i<d.length;i++)d[i].href&&d[i].href.indexOf(e)>-1&&(t=!0);t?o.media=n||"all":setTimeout(r)}var o=window.document.createElement("link"),i=t||window.document.getElementsByTagName("script")[0],d=window.document.styleSheets;return o.rel="stylesheet",o.href=e,o.media="only x",i.parentNode.insertBefore(o,i),r(),o}for(var i in u){loadCSS(u[i]);}}(['http://fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300&subset=latin,cyrillic','http://fonts.googleapis.com/css?family=Open+Sans:400,300,700,800&subset=latin,cyrillic','http://fonts.googleapis.com/css?family=PT+Serif:400,700&subset=latin,cyrillic']));
+	</script>
+<!-- /build -->
+<script>function menu(){document.getElementById('cd-primary-nav').classList.toggle('nav-is-visible');document.getElementById('cd-main-header').classList.toggle('nav-is-visible');return false;}</script>
+</head>
+<body>
+
+<nav class="navbar navbar-default" role="navigation">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="javascript:void(0);bs-example-navbar-collapse-1">
+            <span class="sr-only">Расскрыющаяся навигацая</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+      <a class="navbar-brand" href="javascript:void(0);"><img src="/img/brand.jpg" alt="kiss"></a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="javascript:void(0);">Ссылка</a></li>
+        <li><a href="javascript:void(0);">Ссылка</a></li>
+        <li class="dropdown">
+          <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+          <ul class="dropdown-menu">
+            <li><a href="javascript:void(0);">Действие</a></li>
+            <li><a href="javascript:void(0);">Другое действие</a></li>
+            <li><a href="javascript:void(0);">Что-то еще</a></li>
+            <li class="divider"></li>
+            <li><a href="javascript:void(0);">Отдельная ссылка</a></li>
+            <li class="divider"></li>
+            <li><a href="javascript:void(0);">Еще одна отдельная ссылка</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+<footer>
+
+</footer>
+
+<!-- build:remove -->
+<script src="/libs/jquery/dist/jquery.min.js"></script>
+<script src="/libs/bootstrap-sass/assets/javascripts/bootstrap.js"></script>
+<script src="/libs/mega-site-navigation/js/main.js"></script>
+<script src="/js/common.js"></script>
+<!-- /build -->
+
+<!-- build:template
+<script>
+var ms=document.createElement("link");ms.rel="stylesheet";
+ms.href="/css/style.min.css";document.getElementsByTagName("head")[0].appendChild(ms);
+</script>
+<script>
+var scr = {"scripts":[{"src" : "/js/scripts.min.js", "async" : false}]};
+!function(t,n,r){"use strict";var c=function(t){if("[object Array]"!==Object.prototype.toString.call(t))return!1;for(var r=0;r<t.length;r++){var c=n.createElement("script"),e=t[r];c.src=e.src,c.async=e.async,n.body.appendChild(c)}return!0};t.addEventListener?t.addEventListener("load",function(){c(r.scripts);},!1):t.attachEvent?t.attachEvent("onload",function(){c(r.scripts)}):t.onload=function(){c(r.scripts)}}(window,document,scr);
+</script>
+/build -->
+
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <!--<script src="/js/bootstrap.min.js"></script>-->
+       <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</body>
+</html>
